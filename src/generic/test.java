872970439e5f1492b079f8c50fec9d5ddc662b36
amar <amartanwar93@gmail.com>
2017-06-16 10:50:26 +0530

    First commit

---
+++ src/generic/test.java
@@ -0,0 +1,35 @@
+package generic;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.server.handler.SendKeys;
+
+public class test {
+
+	public static void main(String[] args) throws EncryptedDocumentException, InvalidFormatException, IOException, InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com");
+		File f= new File("./TestData/Input.xlsx");
+		Workbook wb= new WorkbookFactory().create(f);
+		String ws=wb.getSheet("Sheet1").getRow(0).getCell(0).getStringCellValue();
+		System.out.println(ws);
+		driver.findElement(By.id("lst-ib")).sendKeys(ws);
+		Thread.sleep(5000);
+		driver.quit();
+		
+		
+		
+		
+
+	}
+
+}
